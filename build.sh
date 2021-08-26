8685a5aa2619c0ee3e477ecfc814007c014f1ab6
Clara Koehne <clara.koehne@gmx.de>
2021-08-26 12:09:27 +0200

    installation adds link to bin/patchwork

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 
 compile_file="${RECIPE_DIR}/compile.jl"
 build_dir="${SRC_DIR}/build"
+patchwork_dir="patchwork-${PKG_VERSION}"
 
-mkdir -p "${build_dir}/patchwork"
+mkdir -p "${build_dir}/${patchwork_dir}"
 cd "${build_dir}" || return
 
 #julia -e 'using Pkg; Pkg.add(["PackageCompiler"])'
-julia "${compile_file}" "${RECIPE_DIR}" "${build_dir}/patchwork"
+julia "${compile_file}" "${RECIPE_DIR}" "${build_dir}/${patchwork_dir}"
 
 mkdir -p "${PREFIX}/bin"
 cp -r ${build_dir}/* "${PREFIX}/bin"
-#install -d ${PREFIX}/bin
-#install gff ${PREFIX}/bin/
+ln -s "${PREFIX}/bin/${patchwork_dir}/bin/patchwork" "${PREFIX}/bin/patchwork"
+#ln -s "${PREFIX}/bin/patchwork/bin/patchwork" "${PREFIX}/bin/patchwork"
+#chmod u+x ${PREFIX}/bin/patchwork
