294c07f89c29170e053c401714a7d34ae156ae0c
Clara Koehne <clara.koehne@gmx.de>
2021-08-26 12:09:54 +0200

    changed app_name and removed comments

--- compile.jl
+++ compile.jl
@@ -1,18 +1,9 @@
-println("##############################################################################")
-println("START COMPILE ################################################################")
-println("##############################################################################")
 using Pkg
-Pkg.add("PackageCompiler")
 using PackageCompiler
 
-#projectenv = ARGS[1] #"/home/clara/Data/GAU/Work/Projects/Patchwork"
 projectdirectory = ARGS[1]                      # the conda build source directory SRC_DIR
 precompiled = projectdirectory * "/precompiled.jl"
 outdirectory = ARGS[2]                          # the conda build build/patchwork directory
-
-println("##############################################################################")
-println("MAKE OUTDIR ##################################################################")
-println("##############################################################################")
 
 try
     mkpath(outdirectory)
@@ -22,26 +13,7 @@ catch e
     global outdirectory = pwd()
 end
 
-println("##############################################################################")
-println("ACTIVATE ENV #################################################################")
-println("##############################################################################")
 Pkg.activate(projectdirectory)
-println("##############################################################################")
-println("ACTIVATED ####################################################################")
-println("##############################################################################")
 
-#Pkg.activate(projectdirectory)
-#println("environment activated")
-#Pkg.instantiate()
-#println("instantiated")
-
-println("##############################################################################")
-println("CREATE APP ###################################################################")
-println("##############################################################################")
 create_app(projectdirectory, outdirectory; precompile_statements_file = precompiled, 
-           force = true)
-println("##############################################################################")
-println("DONE ######## ################################################################")
-println("##############################################################################")
-
-
+           force = true, app_name = "patchwork")
