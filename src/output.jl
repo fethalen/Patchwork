3e2508d2b70a16488a23f8a59ea81657dfa3a80f
Clara Köhne <clara.koehne@gmx.de>
2021-09-01 10:49:58 +0200

    added warning for overwriting ouput

--- src/output.jl
+++ src/output.jl
@@ -1,34 +1,45 @@
 using BioAlignments
 using FASTX
 
+const WIDTH = 80
+
 function cleanfiles(files::String...)
     for file in files
         isfile(file) && run(pipeline(`rm $file`))
     end
 end
 
+function warn_overwrite()::String
+    println("WARNING: Found output files from a previous run.")
+    println("Do you want to overwrite them? (y/n): ")
+    answer = readline()
+    while !isequal(answer, "y") && !isequal(answer, "n")
+        println("Please type either 'y' for yes or 'n' for no.")
+        answer = readline()
+    end
+    return answer
+end
+
 function write_alignmentfile(file::AbstractString, id::SequenceIdentifier, contigs::Int64,
-                             alignment::BioAlignments.PairwiseAlignment)
+                             alignment::BioAlignments.PairwiseAlignment, index::Int64)
+    count = string(index) * ". "    
     subjectlength = length(alignment.b)
     querylength = length(alignment.a.seq)
     exactmatches = count_matches(alignment)
     mismatches = count_mismatches(alignment)
-    #unspecifed = count(alignment, OP_MATCH)
     deletions = count_deletions(alignment)
     aln_occupancy = Patchwork.occupancy(alignment)
     open(file, "a") do io
-        #if filesize(file) == 0
-        #    print(io, "reference_ID\treference_length\tquery_length\tcontigs\tmatches\t" * 
-        #              "mismatches\tdeletions\toccupancy\n")
-        #end
-        print(io, "reference_ID: " * id.id * "\t")
-        print(io, "reference_length" * string(subjectlength) * "\t")
-        print(io, "query_length" * string(querylength) * "\t")
-        print(io, "contigs: " * string(contigs) * "\t")
-        print(io, "matches: " * string(exactmatches) * "\t")
-        print(io, "mismatches: " * string(mismatches) * "\t")
-        print(io, "deletions: " * string(deletions) * "\t")
-        print(io, "occupancy: " * string(aln_occupancy) * "\n")
+        print(io, count * repeat('-', WIDTH - length(count)) * "\n")
+        print(io, "\n")
+        print(io, "Reference ID:        " * id.id * "\n")
+        print(io, "Reference Length:    " * string(subjectlength) * "\n")
+        print(io, "Query Length:        " * string(querylength) * "\n")
+        print(io, "Contigs:             " * string(contigs) * "\n")
+        print(io, "Matches:             " * string(exactmatches) * "\n")
+        print(io, "Mismatches:          " * string(mismatches) * "\n")
+        print(io, "Deletions:           " * string(deletions) * "\n")
+        print(io, "Occupancy:           " * string(aln_occupancy) * "\n")
         print(io, "\n")
         print(io, alignment)
         print(io, "\n")
