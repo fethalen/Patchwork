528696d96ffcb63da40e37569a0b4083ec50c92a
Clara Köhne <clara.koehne@gmx.de>
2021-11-18 10:32:02 +0100

    remove duplicate sequences from msa object

--- src/filtering.jl
+++ src/filtering.jl
@@ -9,24 +9,42 @@ function remove_duplicates!(searchresults::DataFrame)::DataFrame
     return searchresults
 end
 
-function remove_duplicates(msa::MultipleSequenceAlignment)
-    collection = []
-    for record in msa.sequences
-        record.otu == "Ceratonereis_australis" || continue
-        first = 0
-        last = 0
-        for (index, position) in enumerate(record.sequencedata)
-            if first == 0 && ! isgap(position)
-                first = index
-            elseif last == 0 && first != 0 && isgap(position)
-                last = index
-            end
+function remove_duplicates!(msa::MultipleSequenceAlignment)
+    msa = sort(msa)
+    i = 1
+    while i < lastindex(msa)
+        if isequal(msa[i].sequencedata, msa[i+1].sequencedata) 
+            deleteat!(msa, i)
+        else
+            i += 1
         end
-        println("first: ", first, " last: ", last)
-        push!(collection, AlignedRegion(record, first, last, 1, 100.0))
     end
-    return collection
+    return msa
 end
+
+function remove_duplicates(msa::MultipleSequenceAlignment)
+    result = deepcopy(msa)
+    return remove_duplicates!(result)
+end
+
+# function remove_duplicates(msa::MultipleSequenceAlignment)
+#     collection = []
+#     for record in msa.sequences
+#         record.otu == "Ceratonereis_australis" || continue
+#         first = 0
+#         last = 0
+#         for (index, position) in enumerate(record.sequencedata)
+#             if first == 0 && ! isgap(position)
+#                 first = index
+#             elseif last == 0 && first != 0 && isgap(position)
+#                 last = index
+#             end
+#         end
+#         println("first: ", first, " last: ", last)
+#         push!(collection, AlignedRegion(record, first, last, 1, 100.0))
+#     end
+#     return collection
+# end
 
 # What is BLA?
 # function remove_duplicates(
