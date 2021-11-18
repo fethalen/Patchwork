73db1a3f9beacd3c47c248c139a6fda4bfbd7066
Clara Köhne <clara.koehne@gmx.de>
2021-11-18 10:31:09 +0100

    move isfastafile to fasta.jl

--- src/diamond.jl
+++ src/diamond.jl
@@ -212,12 +212,6 @@ function subjectids(
     return map(result -> result.subjectid.id, results)
 end
 
-function isfastafile(path::AbstractString)::Bool
-    splits = split(path, ".")
-    length(splits) > 1 && last(splits) in FASTAEXTENSIONS && return true
-    return false
-end
-
 function isdiamonddatabase(path::AbstractString)::Bool
     splits = split(path, ".")
     length(splits) > 1 && isequal(last(splits), DIAMONDDB) && return true
