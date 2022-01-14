fbaa6f7fc1c76787d01a50513ada6cda28b829b6
Clara Koehne <clara.koehne@gmx.de>
2022-01-14 16:13:23 +0100

    format nicely...

--- src/clustering.jl
+++ src/clustering.jl
@@ -59,11 +59,11 @@ end
 rotateleft(val, bits) = xor(val << bits, val >> (16 - bits))
 
 function circularhash(kmer::LongDNASeq, k::Int64)
-	# fixed 16bit randoms
+    # fixed 16bit randoms
     rands = Dict(DNA_A => 0x4567, DNA_C => 0x23c6, DNA_G => 0x9869, DNA_T => 0x4873, 
-		DNA_M => 0xdc51, DNA_R => 0x5cff, DNA_W => 0x944a, DNA_S => 0x58ec, DNA_Y => 0x1f29, 
-		DNA_K => 0x7ccd, DNA_V => 0x58ba, DNA_H => 0xd7ab, DNA_D => 0x41f2, DNA_B => 0x1efb, 
-		DNA_N => 0xa9e3, DNA_Gap => 0xe146)
+        DNA_M => 0xdc51, DNA_R => 0x5cff, DNA_W => 0x944a, DNA_S => 0x58ec, DNA_Y => 0x1f29, 
+        DNA_K => 0x7ccd, DNA_V => 0x58ba, DNA_H => 0xd7ab, DNA_D => 0x41f2, DNA_B => 0x1efb, 
+        DNA_N => 0xa9e3, DNA_Gap => 0xe146)
     h = 0x0
     h = xor(h, rands[kmer[1]])
     for i in 2:k
@@ -76,9 +76,9 @@ end
 # rolling hash variant for previous hash function
 function circularhash_next(seq::LongDNASeq, k::Int64, prevbase::DNA, h::UInt16)
     rands = Dict(DNA_A => 0x4567, DNA_C => 0x23c6, DNA_G => 0x9869, DNA_T => 0x4873, 
-		DNA_M => 0xdc51, DNA_R => 0x5cff, DNA_W => 0x944a, DNA_S => 0x58ec, DNA_Y => 0x1f29, 
-		DNA_K => 0x7ccd, DNA_V => 0x58ba, DNA_H => 0xd7ab, DNA_D => 0x41f2, DNA_B => 0x1efb, 
-		DNA_N => 0xa9e3, DNA_Gap => 0xe146)
+        DNA_M => 0xdc51, DNA_R => 0x5cff, DNA_W => 0x944a, DNA_S => 0x58ec, DNA_Y => 0x1f29, 
+        DNA_K => 0x7ccd, DNA_V => 0x58ba, DNA_H => 0xd7ab, DNA_D => 0x41f2, DNA_B => 0x1efb, 
+        DNA_N => 0xa9e3, DNA_Gap => 0xe146)
     h = xor(h, rotateleft(rands[prevbase], (5 * k) % 16))
     h = rotateleft(h, 5)
     h = xor(h, rands[seq[k]])
@@ -92,17 +92,17 @@ mutable struct KmerIndex
     kmerpos::Int64
     msaindex::Int64 # reference to id and sequence in corresponding msa
 
-	function KmerIndex(kmer::LongDNASeq, seqlen::Int64, kmerpos::Int64, msaindex::Int64)
+    function KmerIndex(kmer::LongDNASeq, seqlen::Int64, kmerpos::Int64, msaindex::Int64)
         return new(kmer, seqlen, kmerpos, msaindex)
     end
 end
 
 function KmerIndex()
-	return KmerIndex(LongDNASeq(), 0, 0, 0)
+    return KmerIndex(LongDNASeq(), 0, 0, 0)
 end
 
 function KmerIndex(kmer::LongDNASeq, seq::LongDNASeq, kmerpos::Int64, msaindex::Int64)
-	return KmerIndex(kmer, length(seq), kmerpos, msaindex)
+    return KmerIndex(kmer, length(seq), kmerpos, msaindex)
 end
 
 Base.isempty(kmerindex::KmerIndex) = isempty(kmerindex.kmer)
@@ -111,65 +111,65 @@ Base.isempty(kmerindex::KmerIndex) = isempty(kmerindex.kmer)
 # then you can use the following functions sequentially for doing the clustering: 
 
 function buildkmertable(
-	name::AbstractString, 
-	reader::FASTA.Reader, 
-	k::Int64, m::Int64, 
-	filelength::Int64=10000000
+    name::AbstractString, 
+    reader::FASTA.Reader, 
+    k::Int64, m::Int64, 
+    filelength::Int64=10000000
 )
     record = FASTA.Record()
     all_kmertable = repeat([Vector{KmerIndex}()], filelength)
-	sequences = repeat([SequenceRecord()], filelength)
+    sequences = repeat([SequenceRecord()], filelength)
     eof(reader) && return kmertable
     msaindex = 1
 
     while !eof(reader)
         read!(reader, record)
-		seq = FASTX.sequence(record)
-		seqlen = length(seq)
+        seq = FASTX.sequence(record)
+        seqlen = length(seq)
 
-		# no need to relocate the array for storing new records: array was preallocated
-		sequences[msaindex] = SequenceRecord(FASTX.identifier(record), seq) 
+        # no need to relocate the array for storing new records: array was preallocated
+        sequences[msaindex] = SequenceRecord(FASTX.identifier(record), seq) 
         # looking for a max of m lowest-hashing kmers
         num_kmers = m + k - 1 <= seqlen ? m : seqlen - k + 1
-		#kmertable = repeat([KmerIndex()], num_kmers)
-		kmertable = Vector{KmerIndex}(undef, num_kmers)
-		# [(kmer, hash, kmerpos),...]
-		seq_kmers = Vector{Tuple{LongDNASeq, UInt16, Int64}}(undef, seqlen - k + 1)
-		seq_kmers[1] = (seq[1:k], circularhash(seq[1:k], k), 1)
+        #kmertable = repeat([KmerIndex()], num_kmers)
+        kmertable = Vector{KmerIndex}(undef, num_kmers)
+        # [(kmer, hash, kmerpos),...]
+        seq_kmers = Vector{Tuple{LongDNASeq, UInt16, Int64}}(undef, seqlen - k + 1)
+        seq_kmers[1] = (seq[1:k], circularhash(seq[1:k], k), 1)
 
         for i in 2:seqlen - k + 1 # compute hashes for all kmers
-			seq_kmers[i] = (seq[i:i+k-1],
-        		circularhash_next(seq, i+k-1, seq[i-1], seq_kmers[i-1][2]), i)
+            seq_kmers[i] = (seq[i:i+k-1],
+                circularhash_next(seq, i+k-1, seq[i-1], seq_kmers[i-1][2]), i)
         end
 
         if length(seq_kmers) > m # store the m lowest-hashing kmers in the index table
-        	for i in 1:m
-        		minhash, idx = findmin(map(tup -> tup[2], seq_kmers))
-				kmertable[i] = KmerIndex(seq_kmers[idx][1], seqlen, seq_kmers[idx][3], 
-					msaindex)
-				if minhash == 0xffff
-					deleteat!(seq_kmers, idx)
-				else # avoid relocating m * ~l array entries where possible
-					seq_kmers[idx] = (seq_kmers[idx][1], 0xffff, seq_kmers[idx][3]) 
-				end
-        	end
+            for i in 1:m
+                minhash, idx = findmin(map(tup -> tup[2], seq_kmers))
+                kmertable[i] = KmerIndex(seq_kmers[idx][1], seqlen, seq_kmers[idx][3], 
+                    msaindex)
+                if minhash == 0xffff
+                    deleteat!(seq_kmers, idx)
+                else # avoid relocating m * ~l array entries where possible
+                    seq_kmers[idx] = (seq_kmers[idx][1], 0xffff, seq_kmers[idx][3]) 
+                end
+            end
         else # only <= m kmers in total
-        	for (i, tup) in enumerate(seq_kmers)
-				kmertable[i] = KmerIndex(tup[1], seqlen, tup[3], msaindex)
-        	end
+            for (i, tup) in enumerate(seq_kmers)
+                kmertable[i] = KmerIndex(tup[1], seqlen, tup[3], msaindex)
+            end
         end
-		all_kmertable[msaindex] = kmertable
+        all_kmertable[msaindex] = kmertable
         msaindex += 1
     end
-	sequences = MultipleSequenceAlignment(name, sequences)
-	all_kmertable = collect(Iterators.flatten(all_kmertable)) # [...] instead of [[...],...]
+    sequences = MultipleSequenceAlignment(name, sequences)
+    all_kmertable = collect(Iterators.flatten(all_kmertable)) # [...] instead of [[...],...]
     return sequences, all_kmertable
 end
 
 function buildkmertable(
-	file::AbstractString, 
-	k::Int64, m::Int64, 
-	filelength::Int64=10000000
+    file::AbstractString, 
+    k::Int64, m::Int64, 
+    filelength::Int64=10000000
 )
     reader = FASTA.Reader(open(file))
     return buildkmertable(file, reader, k, m, filelength)
@@ -186,53 +186,53 @@ function mktemp_kmertable(kmertable::Vector{KmerIndex})
         close(tmpio)
         return tmpfile
     end
-	writestart = 1
+    writestart = 1
     currentcenter = kmertable[writestart] # ===
     i = 2
-	countlines = 0
+    countlines = 0
     while i <= length(kmertable)
         if !isequal(kmertable[i].kmer, currentcenter.kmer) # entered new kmer block in sorted kmer table
-			if writestart < i-1 # don't write trivial/1-sequence blocks
-	            write(tmpio, string(currentcenter.msaindex)) # write center'\t's1'\t's2'\t's3'\t'...'\t'sn'\n'
-    	        for j in writestart+1:i-1 # write the entire previous group to file
-					if !isempty(kmertable[j]) # reduntant entries were marked empty instead of deleted
-        	        	write(tmpio, "\t" * string(kmertable[j].msaindex))
-					end
-            	end
-            	write(tmpio, "\n") # group end --> new line
-				countlines += 1
-			end
-			writestart = i # current position is new kmer group start
+            if writestart < i-1 # don't write trivial/1-sequence blocks
+                write(tmpio, string(currentcenter.msaindex)) # write center'\t's1'\t's2'\t's3'\t'...'\t'sn'\n'
+                for j in writestart+1:i-1 # write the entire previous group to file
+                    if !isempty(kmertable[j]) # reduntant entries were marked empty instead of deleted
+                        write(tmpio, "\t" * string(kmertable[j].msaindex))
+                    end
+                end
+                write(tmpio, "\n") # group end --> new line
+                countlines += 1
+            end
+            writestart = i # current position is new kmer group start
             currentcenter = kmertable[writestart] # current position is new center sequence; ===
             i += 1
         else # still in same kmer block, sequence is not the current center
             kmertable[i].kmerpos = currentcenter.kmerpos - kmertable[i].kmerpos # overwrite absolute with relative position
             if kmertable[i].msaindex == kmertable[i-1].msaindex # same sequence has more than one matching kmer with center
-				# keep only entry with smaller relative kmer pos
-				# make sure to keep currentcenter (writestart), if current center seq has same kmer in diff pos
+                # keep only entry with smaller relative kmer pos
+                # make sure to keep currentcenter (writestart), if current center seq has same kmer in diff pos
                 if kmertable[i].kmerpos >= kmertable[i-1].kmerpos || writestart == i-1 
                     # deleteat!(kmertable, i)
-					kmertable[i] = KmerIndex() # avoid relocating the array
+                    kmertable[i] = KmerIndex() # avoid relocating the array
                 else
                     # deleteat!(kmertable, i - 1)
-					kmertable[i-1] = KmerIndex()
+                    kmertable[i-1] = KmerIndex()
                 end
             # else
             #     i += 1 # no entry deleted --> next seq == next index
             end
-			i += 1 # entries aren't deleted but just emptied, so it's always next seq == next index
+            i += 1 # entries aren't deleted but just emptied, so it's always next seq == next index
         end
     end
     if writestart < i-1 # don't write trivial/1-sequence blocks
-		write(tmpio, string(currentcenter.msaindex)) # write center'\t's1'\t's2'\t's3'\t'...'\t'sn'\n'
-		for j in writestart+1:i-1 # write the entire previous group to file
-			if !isempty(kmertable[j]) # reduntant entries were marked empty instead of deleted
-				write(tmpio, "\t" * string(kmertable[j].msaindex))
-			end
-		end
-		write(tmpio, "\n") # group end --> new line
-		countlines += 1
-	end
+        write(tmpio, string(currentcenter.msaindex)) # write center'\t's1'\t's2'\t's3'\t'...'\t'sn'\n'
+        for j in writestart+1:i-1 # write the entire previous group to file
+            if !isempty(kmertable[j]) # reduntant entries were marked empty instead of deleted
+                write(tmpio, "\t" * string(kmertable[j].msaindex))
+            end
+        end
+        write(tmpio, "\n") # group end --> new line
+        countlines += 1
+    end
     close(tmpio)
     return tmpfile, countlines # number of centers == number of lines
 end
@@ -240,41 +240,41 @@ end
 # ungapped alignment filter
 function gapfreealign_tocenter(kmertable_file::AbstractString, filelength::Int64, msa::MultipleSequenceAlignment, t::Float64)
     # edges = Vector{Int64}[] # [[center idx, s1 idx, s2 idx, ...],...]
-	edges = repeat([Vector{Int64}()], filelength) # [[center idx, s1 idx, s2 idx, ...],...]
-	i = 1
+    edges = repeat([Vector{Int64}()], filelength) # [[center idx, s1 idx, s2 idx, ...],...]
+    i = 1
     open(kmertable_file) do reader
         while !eof(reader)
-			@assert i <= filelength "Some error in counting lines"
-			newcluster = true
+            @assert i <= filelength "Some error in counting lines"
+            newcluster = true
             positions = map(x -> parse(Int64, x), split(readline(reader), "\t")) # center msaindex, sequence msaindex
             for seqindex in positions[2:end]
                 aln = gapfreealign_t(msa[positions[1]], msa[seqindex], t)
-				if aln
-					if newcluster 
-						# push!(edges, [positions[1], seqindex])
-						edges[i] = [positions[1], seqindex]
-						newcluster = false
-					else
-						# push!(edges[end], seqindex)
-						push!(edges[i], seqindex)
-					end
-				end
+                if aln
+                    if newcluster 
+                        # push!(edges, [positions[1], seqindex])
+                        edges[i] = [positions[1], seqindex]
+                        newcluster = false
+                    else
+                        # push!(edges[end], seqindex)
+                        push!(edges[i], seqindex)
+                    end
+                end
             end
-			if !newcluster # at least one alignment was found for this index --> index++
-				i += 1
-			end
+            if !newcluster # at least one alignment was found for this index --> index++
+                i += 1
+            end
         end
     end
-	sort!(edges, by = vec -> length(vec), rev = true) # sort by decreasing cluster size
-	tmpfile, tmpio = mktemp()
+    sort!(edges, by = vec -> length(vec), rev = true) # sort by decreasing cluster size
+    tmpfile, tmpio = mktemp()
     # for edge in edges
     #     write(tmpio, join(string.(edge), "\t") * "\n")
     # end
-	i = 1
-	while !isempty(edges[i])
-		write(tmpio, join(string.(edges[i]), "\t") * "\n")
-		i += 1
-	end
+    i = 1
+    while !isempty(edges[i])
+        write(tmpio, join(string.(edges[i]), "\t") * "\n")
+        i += 1
+    end
     close(tmpio)
     return tmpfile
 end
@@ -284,12 +284,12 @@ function greedyincremental_clustering(edgesfile::AbstractString, msa::MultipleSe
     open(edgesfile) do reader
         while !eof(reader)
             edge = map(x -> parse(Int64, x), split(readline(reader), "\t"))
-			# if this center doesn't belong to a different, larger cluster: remove all its neighbours
-			if keepers[edge[1]] 
-            	for seqindex in edge[2:end]
-					keepers[seqindex] = false
-				end
-			end # else, keep them: they're orphaned, the cluster center was already removed
+            # if this center doesn't belong to a different, larger cluster: remove all its neighbours
+            if keepers[edge[1]] 
+                for seqindex in edge[2:end]
+                    keepers[seqindex] = false
+                end
+            end # else, keep them: they're orphaned, the cluster center was already removed
         end
     end
     msa = MultipleSequenceAlignment(msa.name, msa[findall(keepers)])
