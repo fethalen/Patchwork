92aa7c014f7d69ac4febcf9306bc692959cd88c1
Felix Thalen <felix.thalen@gmail.com>
2022-02-16 16:11:58 +0100

    fixed reader not defined error

--- src/compile.jl
+++ src/compile.jl
@@ -1,12 +1,12 @@
-# this script can be run on its own, e.g. from inside the Patchwork directory: 
+# this script can be run on its own, e.g. from inside the Patchwork directory:
 # julia --trace-compile="src/precompiled.jl" src/Patchwork.jl --contigs CONTIGSFILE --reference REFERENCEFILE
 # julia src/compile.jl . src/precompiled.jl ../patchwork
 # or run it as part of conda build, from inside the Patchwork recipe directory
 # that contains the meta.yaml, build.sh, Project.toml and Manifest.toml files:
-# (no precompilation necessary) 
+# (no precompilation necessary)
 # conda build .
 
-import Pkg 
+import Pkg
 Pkg.add(name="PackageCompiler", version="1.7.1")
 using PackageCompiler
 
@@ -16,5 +16,5 @@ precompiled = ARGS[2]
 outdirectory = ARGS[3]
 
 println("CREATE")
-create_app(projectdirectory, outdirectory; precompile_statements_file = precompiled, 
+create_app(projectdirectory, outdirectory, precompile_statements_file = precompiled,
            force = true, app_name = "patchwork")
