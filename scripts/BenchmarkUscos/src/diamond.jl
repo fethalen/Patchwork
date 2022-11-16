1abf3e16cc0bc99a074dae958797c20aade8f65d
Clara Köhne <clara.koehne@gmx.de>
2022-11-16 11:24:11 +0100

    integrate --frameshift option for DNA queries

--- scripts/BenchmarkUscos/src/diamond.jl
+++ scripts/BenchmarkUscos/src/diamond.jl
@@ -55,34 +55,37 @@ function readblastTSV(path::AbstractString; dnaquery::Bool=false)::Array{Diamond
                 row.cigar, row.pident, row.bitscore
             )
         else
-            @assert length(row.qseq) % 3 == 0 """
-                qseq:\n $(row.qseq)\nlength:\n $(length(row.qseq)).\n 
-                Not divisible by three. 
-                """
+            # if no frameshift: 
+            # @assert length(row.qseq) % 3 == 0 """
+            #     qseq:\n $(row.qseq)\nlength:\n $(length(row.qseq)).\n 
+            #     Not divisible by three. 
+            #     """
 
             # ASK FELIX: QUERYSEQUENCE TRANSLATION BY JULIA AND DIAMOND ARE DIFFERENT; 
             # WHICH ONE TO USE? 
             # IF DIAMOND, NEED TO "PATCH" THE FULL QUERY SEQUENCE TOGETHER FROM THE 
             # DIAMOND-TRANSLATED PART + JULIA-TRANSLATED UNALIGNED REGIONS 
 
-            # querysequence = BioSequences.translate(LongDNA{4}(row.qseq))    # this should work, providing no frameshifting (default) happened in the DIAMOND BLASTX search
+            # # querysequence = BioSequences.translate(LongDNA{4}(row.qseq))    # this should work, providing no frameshifting (default) happened in the DIAMOND BLASTX search
 
             # # @assert isequal(String(querysequence), String(row.qseq_translated)) """
             # #     julia translation:\n $querysequence\nDIAMOND translation:\n $(row.qseq_translated).\n
             # #     DNA query:\n $(row.qseq).\n
             # #     """
 
-            # if length(row.full_qseq) % 3 != 0
-            #     trimmed = trim(row.full_qseq, row.qseq)
-            #     @assert length(trimmed) % 3 == 0 """
-            #         trimmed full_qseq:\n $(trimmed)\nlength:\n $(length(trimmed)).\n 
-            #         Not divisible by three. 
-            #         """
-            #     fullquery = BioSequences.translate(LongDNA{4}(trimmed))
-            # else
-            #     fullquery = BioSequences.translate(LongDNA{4}(row.full_qseq))   # (default) happened in the DIAMOND BLASTX search
-            # end
+            # # if length(row.full_qseq) % 3 != 0
+            # #     trimmed = trim(row.full_qseq, row.qseq)
+            # #     @assert length(trimmed) % 3 == 0 """
+            # #         trimmed full_qseq:\n $(trimmed)\nlength:\n $(length(trimmed)).\n 
+            # #         Not divisible by three. 
+            # #         """
+            # #     fullquery = BioSequences.translate(LongDNA{4}(trimmed))
+            # # else
+            # #     fullquery = BioSequences.translate(LongDNA{4}(row.full_qseq))   # (default) happened in the DIAMOND BLASTX search
+            # # end
 
+            # querysequence = translate_fromcigar(LongDNA{4}(row.qseq), row.cigar)  # julia-translation if frameshifting is allowed in DIAMOND
+            # fullquery = patch_translate_fullquery(row.full_qseq, row.qseq, querysequences)
             querysequence = LongAA(row.qseq_translated)
             fullquery = patch_translate_fullquery(row.full_qseq, row.qseq, row.qseq_translated)
 
@@ -111,8 +114,19 @@ function trim(full_qseq::AbstractString, qseq::AbstractString)::AbstractString
         error("Query is not a subsequence of Full Query.")
     end
     startidx = (first(subseqidx) % 3) == 0 ? 3 : (first(subseqidx) % 3)
-    newlength = length(full_qseq) - startidx + 1
-    endidx = length(full_qseq) - (newlength%3)
+    
+    #newlength = length(full_qseq) - startidx + 1
+    #endidx = (last(subseqidx) == lastindex(full_qseq)) ? last(subseqidx) : length(full_qseq) - (newlength%3)
+
+    lengthafterqseq = lastindex(full_qseq) - last(subseqidx)
+    @assert lengthafterqseq >= 0 """
+        query:\n $(qseq)\n 
+        full query:\n $(full_qseq)\n 
+        Query ends after Full Query. 
+        """
+
+    endidx = last(subseqidx) + (lengthafterqseq - lengthafterqseq%3)
+    #println("first: ", startidx, " last: ", endidx, " len -qseq: ", length(full_qseq[startidx:endidx])-length(qseq))
     return full_qseq[startidx:endidx]
 end
 
@@ -123,8 +137,28 @@ and the julia-translated "rest" of the full query sequence (i.e. the parts that 
 succeed the part of the full sequence that corresponds to `qseq_translated`).
 """
 function patch_translate_fullquery(full_qseq::AbstractString, qseq::AbstractString, qseq_translated::AbstractString)::LongAA
+    translatedseq = LongAA(qseq_translated)
+    return patch_translate_fullquery(full_qseq, qseq, translatedseq)
+end
+
+function patch_translate_fullquery(full_qseq::AbstractString, qseq::AbstractString, qseq_translated::LongAA)::LongAA
+    if isequal(full_qseq, qseq) #length(qseq) >= length(full_qseq)
+        return qseq_translated
+    elseif length(full_qseq) <= length(qseq)
+        println("Query:")
+        println(qseq)
+        println("Full Query:")
+        println(full_qseq)
+        error("lengths Query <= Full Query; and sequences not equal.")
+    end
+    
+    old_seq = full_qseq
     full_qseq = trim(full_qseq, qseq)
-    @assert length(full_qseq) % 3 == 0 """
+
+    # substract length(qseq) from length(full_qseq) 
+    # bc with frameshift, length(qseq) not necessarily divisible by 3
+    # but length of rest of full_qseq should be divisible by 3
+    @assert (length(full_qseq)-length(qseq)) % 3 == 0 """
         trimmed full_qseq:\n $(full_qseq)\nlength:\n $(length(full_qseq)).\n
         untrimmed full_qseq:\n $(old_seq)\nlength:\n $(length(old_seq)).\n 
         New length not divisible by three. 
@@ -136,8 +170,6 @@ function patch_translate_fullquery(full_qseq::AbstractString, qseq::AbstractStri
         full query:\n $(full_qseq)\n 
         Query is not a subsequence of full query. 
         """
-
-    translatedseq = LongAA(qseq_translated)
 
     if first(translatedpart) > firstindex(full_qseq)
         firstpart = firstindex(full_qseq):(first(translatedpart)-1)
@@ -152,8 +184,38 @@ function patch_translate_fullquery(full_qseq::AbstractString, qseq::AbstractStri
         lastseq = LongAA()
     end
 
-    return firstseq * translatedseq * lastseq
+    return firstseq * qseq_translated * lastseq
 end
+
+# in case frameshifting operations are allowed in DIAMOND (--framshift 15)
+# not needed if patch_translate_fullquery() (i.e., using DIAMOND translation
+# qseq_translated of aligned part qseq)
+# would only be needed for julia-translating qseq
+# function translate_fromcigar(
+#     sequence::BioSequences.LongDNA,
+#     cigar::AbstractString
+# )::BioSequences.LongAA
+#     anchors = collect(eachmatch(r"[/\\MIDNSHP=X]", cigar))
+#     positions = collect(eachmatch(r"\d+", cigar))
+#     index = 1
+#     buffer = IOBuffer()
+
+#     for (p, anchor) in zip(positions, anchors)
+#         pos = parse(Int64, p.match)
+#         # anchor corresponding to pos * 1 nucleotide
+#         if isequal(anchor.match, "\\")
+#             index += pos
+#         elseif isequal(anchor.match, "/")
+#             index -= pos
+#         # anchor corresponding to pos * nucleotide triplet (if DELETE, don't change index):
+#         elseif !BioAlignments.isdeleteop(BioAlignments.Operation(anchor.match[1]))
+#             print(buffer, sequence[index:index + 3 * pos - 1])
+#             index += 3 * pos
+#         end
+#     end
+
+#     return BioSequences.translate(BioSequences.LongDNA(String(take!(buffer))))
+# end
 
 """
 Runs `diamond makedb` on the provided `reference` FASTA sequence. May include
