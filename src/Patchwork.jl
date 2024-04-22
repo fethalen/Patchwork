207f0378a1f21b7b8ebedd744f79d7a794af7f7f
Felix Thalén <felix.thalen@gmail.com>
2024-04-22 12:36:41 +0200

    version bump for block size option

--- src/Patchwork.jl
+++ src/Patchwork.jl
@@ -120,7 +120,7 @@ const DIAMONDOUTPUT = "diamond_out"
 const STATSOUTPUT = "sequence_stats"
 const PLOTSOUTPUT = "plots"
 const RULER = repeat('─', 74)
-const VERSION = "0.6.3"
+const VERSION = "0.6.4"
 # Default scoremodel taken from DIAMOND's defaults for BLOSUM62
 const DEFAULT_SCOREMODEL = BioAlignments.AffineGapScoreModel(BioAlignments.BLOSUM62,
     gap_open = -11, gap_extend = -1)
