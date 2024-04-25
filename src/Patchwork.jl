d6bef38f1e98b96a3f96243e627a07780b700c29
Felix Thalén <felix.thalen@gmail.com>
2024-04-25 11:42:12 +0200

    version bump

--- src/Patchwork.jl
+++ src/Patchwork.jl
@@ -120,7 +120,7 @@ const DIAMONDOUTPUT = "diamond_out"
 const STATSOUTPUT = "sequence_stats"
 const PLOTSOUTPUT = "plots"
 const RULER = repeat('─', 74)
-const VERSION = "0.6.9"
+const VERSION = "0.7.0"
 # Default scoremodel taken from DIAMOND's defaults for BLOSUM62
 const DEFAULT_SCOREMODEL = BioAlignments.AffineGapScoreModel(BioAlignments.BLOSUM62,
     gap_open = -11, gap_extend = -1)
